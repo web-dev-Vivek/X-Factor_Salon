30aaec4ff8d8b89e4518d7a40d433c733c86d5cd
Vivek Jha <progamervivek2020@gmail.com>
2025-09-27 09:23:38 +0530

    .

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function Experience() {
-  return <div className="h-[50vh] w-screen "></div>;
+  return (
+    <div className="h-[50vh] w-screen bg-red-600">
+      <div>
+        <div></div>
+        <div>185+</div>
+        <div>Location Pan India</div>
+      </div>
+      <div>
+        <div></div>
+        <div>4,000+</div>
+        <div>Experts and Technicians</div>
+      </div>
+      <div>
+        <div></div>
+        <div>4.5</div>
+        <div>Decades of Excellence</div>
+      </div>
+      <div>
+        <div></div>
+        <div>16+</div>
+        <div>Global Awards & Recognition</div>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
